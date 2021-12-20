ef1e066b87dca7a087795deba2b98ddd86697984
Chris Palka <cpalka87gmail.com>
2021-12-20 13:57:29 -0700

    Remove unused dependencies

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const { PORT } = process.env || 5000;
-
-const app = express();
-
-app.listen(PORT, () => console.log(`Connected at: http://localhost:${PORT}`));
